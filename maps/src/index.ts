6dfd80b80869a60f8550aeeb0f02c88f7bee0f8a
David White <dwhite0627@gmail.com>
2020-12-12 18:18:01 -0500

    Add the google maps integration to index.ts

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,11 @@
-import { Company } from './Company';
-import { User } from './User';
+// import { Company } from './Company';
+// import { User } from './User';
 
-const user = new User();
-const company = new Company();
-
-console.log(user)
-console.log(company)
+const mapDiv = document.querySelector('#map');
+new google.maps.Map(mapDiv, {
+  zoom: 2,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+})
