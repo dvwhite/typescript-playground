fcc9154044f3241270cec9527f61105a6bc80a59
David White <dwhite0627@gmail.com>
2020-12-15 12:37:23 -0500

    Add an addMarker method to CustomMap

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,14 +1,15 @@
+
+// Used as an argument to addMarker
 interface Mappable {
   location: {
     lat: number;
     lng: number;
   }
 }
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
-  constructor() {
-    this.googleMap = new google.maps.Map(document.querySelector('#map'), {
   constructor(divId: string) {
     this.googleMap = new google.maps.Map(document.querySelector(`#${divId}`), {
       zoom: 2,
@@ -17,5 +18,15 @@ export class CustomMap {
         lng: 0
       }
     });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    })
   }
 }
