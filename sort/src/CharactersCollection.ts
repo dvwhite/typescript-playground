a864267a580e6a2b79d82ee8b00c8a2bc4bdf40e
David White <dwhite0627@gmail.com>
2021-01-30 18:14:17 -0500

    Add the CharactersCollection class for use in sorting strings

---
+++ sort/src/CharactersCollection.ts
@@ -0,0 +1,21 @@
+export class CharactersCollection {
+  constructor(public data: string) {
+
+  }
+
+  get length() {
+    return this.data.length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase();
+  }
+
+  swap(leftIndex: number, rightIndex: number): void{
+    const chars = this.data.split("");
+    const leftHand = chars[leftIndex];
+    chars[leftIndex] = chars[rightIndex];
+    chars[rightIndex] = leftHand;
+    this.data = chars.join(""); 
+  }
+}
