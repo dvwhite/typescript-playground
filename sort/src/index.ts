070546c787657ea47280cb5c12343da79e10200e
David White <dwhite0627@gmail.com>
2021-01-30 18:16:20 -0500

    Add the CharactersCollection class to index.ts

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,12 +1,14 @@
 // Class imports
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const numSorter = new Sorter(numbersCollection);
 numSorter.sort();
 console.log(numSorter.collection.data);
 
-// const strSorter = new Sorter("ZYXzyxba");
-// strSorter.sort();
-// console.log(strSorter.collection);
+const charactersCollection = new CharactersCollection("Xaayb");
+const strSorter = new Sorter(charactersCollection);
+strSorter.sort();
+console.log(charactersCollection.data);
