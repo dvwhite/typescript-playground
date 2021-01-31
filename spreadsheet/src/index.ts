a0fd7959766525fc1dbddce2559da3d6e110593f
David White <dwhite0627@gmail.com>
2021-01-31 09:41:47 -0500

    Setup the spreadsheet project

---
+++ spreadsheet/src/index.ts
@@ -0,0 +1,11 @@
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+})
+.split('\n')
+.map((row: string): string[] => {
+  return row.split(',');
+});
+
+console.log(matches)
