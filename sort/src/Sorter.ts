3e48346f8c4307ef5ef30336fa6f601a0fb92a87
David White <dwhite0627@gmail.com>
2021-01-31 07:51:36 -0500

    Refactor Sorter to an abstract class

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -4,18 +4,17 @@ interface Sortable {
   swap(leftIndex: number, rightIndex: number): void;
 }
 
-export class Sorter {
-  constructor(public collection: Sortable) {
-  }
+export abstract class Sorter {
+  abstract length: number;
+  abstract compare(leftIndex: number, rightIndex: number): boolean;
+  abstract swap(leftIndex: number, rightIndex: number): void;
 
   sort(): void {
-    const { length } = this.collection;
-
     // Use a type guard to index errors with logic when working with strings
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if(this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if(this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
