2781a8fc57de46b8947fbd742a0fbfacfa87ad99
David White <dwhite0627@gmail.com>
2021-01-31 07:15:50 -0500

    Add the newly created LinkedList to the index.ts

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -2,13 +2,25 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const numSorter = new Sorter(numbersCollection);
 numSorter.sort();
-console.log(numSorter.collection.data);
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Xaayb");
 const strSorter = new Sorter(charactersCollection);
 strSorter.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.print()
+
+const sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();
