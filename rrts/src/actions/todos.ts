64f22544ac48f7a8dbf38f3178bcbb34829669b2
David White <dwhite0627@gmail.com>
2021-02-06 21:34:25 -0500

    Move the todo action creators and interfaces to todos.ts

---
+++ rrts/src/actions/todos.ts
@@ -0,0 +1,34 @@
+// All interfaces and action creators for todos
+
+import axios from 'axios';
+import { Action, Dispatch } from 'redux';
+
+// Action types
+import { ActionTypes } from './types';
+
+// Interfaces
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
+
+// Get the API todo data
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+export const fetchTodos = () => {
+  return async (dispatch: Dispatch) => {
+    const response = await axios.get<Todo[]>(url);
+
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
+      payload: response.data
+    });
+  };
+};
